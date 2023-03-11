970208db169e5a9431f1696094566e3433caee2b
Ralph Magnette <ralphmagnette@hotmail.com>
2023-03-11 16:58:55 +0100

    Finishing Laravel course from Brad Traversy

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use App\Models\Listing;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // User::factory(5)->create();
+
+        $user = User::factory()->create([
+            'name' => 'John Doe',
+            'email' => 'john@gmail.com'
+        ]);
+
+        Listing::factory(6)->create([
+            'user_id' => $user->id
+        ]);
+
+        // Listing::create([
+        //     'title' => 'Laravel Senior Developer',
+        //     'tags' => 'laravel, javascript',
+        //     'company' => 'Acme Corp',
+        //     'location' => 'Boston, MA',
+        //     'email' => 'email@email.com',
+        //     'website' => 'http://www.acme.com',
+        //     'description' => 'Lorem ipsum dolor sit
+        //     amet, consectetur adipiscing elit. Ipsam
+        //     minima et illo reprehenderit quas possimus
+        //     voluptas repudiandae cum expedita, eveniet
+        //     aliquid, quam illum quaerat consequatur!
+        //     Expedita ab consectetur tenetur delesiti?'
+        // ]);
+
+        // Listing::create([
+        //     'title' => 'Full-stack Engineer',
+        //     'tags' => 'laravel, backend, api',
+        //     'company' => 'Stark Industries',
+        //     'location' => 'New York, NY',
+        //     'email' => 'email2@email.com',
+        //     'website' => 'http://www.starkingindustries.com',
+        //     'description' => 'Lorem ipsum dolor sit
+        //     amet, consectetur adipiscing elit. Ipsam
+        //     minima et illo reprehenderit quas possimus
+        //     voluptas repudiandae cum expedita, eveniet
+        //     aliquid, quam illum quaerat consequatur!
+        //     Expedita ab consectetur tenetur delesiti?'
+        // ]);
+    }
+}
